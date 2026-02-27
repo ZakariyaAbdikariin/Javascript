870eb847ee2b386562b1749ad521da6374c61d44
ZakariyaAbdikariin <sak.abdikariin@gmail.com>
2026-02-27 10:10:41 +0000

    Add variables and data types examples in JavaScript

---
+++ variables_data_types/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+var firstName = "Sakariye" //string
+var lastName = "Abdikariin" //string
+ var age = 35 //number
+var isStudent = true //boolean
+console.log(firstName);
+console.log(lastName);
+console.log(age * age);
+console.log(isStudent);
---
+++ variables_data_types/variables.js
@@ -0,0 +1,16 @@
+var firstName = "Sakariye"; //string
+var lastName = "Abdikariin"; //string
+var age = 35; //number
+var isStudent = true; //boolean
+console.log(firstName);
+console.log(lastName);
+console.log(age * age);
+console.log(isStudent);
+
+// var CapitalCity = null;
+// // city = "London"
+// // city = "Manchester"
+// console.log(CapitalCity);
+
+// var theBestThingInTheWorld = 5;
+// console.log(typeof theBestThingInTheWorld);
